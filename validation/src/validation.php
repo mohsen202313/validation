d8e1c80f320a79376cc96af0f032e7323112a5cf
mohsen <mohsensheykholeslami.dev@gmail.com>
2024-12-04 17:44:36 +0330

    v1

--- validation/src/validation.php
+++ validation/src/validation.php
@@ -2,7 +2,7 @@
 namespace mohsenValidation;
 class validation
 {
-    public function require(array $data,$filed) : bool
+    public function has(array $data,$filed) : bool
     {
         return isset($data[$filed]);
     }
